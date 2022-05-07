5bc4d30373451a35d23d2ce4e3fc1736fd4302f4
Yash Chhatrala <yashkansara0425@gmail.com>
2022-05-07 17:03:56 +0530

    video7

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { About } from "./components/About";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { OrderSummary } from "./components/OrderSummary";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
       </Routes>
     </>
   );
--- react_router/src/components/Home.js
+++ react_router/src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
+  const navigate = useNavigate()
   return (
     <div>
         <h1>Home Page</h1>
+        <button onClick={() => navigate('order-summary')}>Place Order</button>
     </div>
   )
 }
