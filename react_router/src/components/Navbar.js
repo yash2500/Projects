d2ee5e40d6d25ead173ac94f1a4a9d3c64d12b12
Yash Chhatrala <yashkansara0425@gmail.com>
2022-05-07 16:51:20 +0530

    video5

--- react_router/src/components/Navbar.js
+++ react_router/src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const navLinkStyles = ({ isActive }) => {
+        return {
+            fontWeight: isActive ? 'bold' : 'normal',
+            color: isActive ? 'aliceblue' : 'whitesmoke'
+        }
+    }
+
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
+        <NavLink style={navLinkStyles} to='/'>Home</NavLink>
+        <NavLink style={navLinkStyles} to='/about'>About</NavLink>
     </nav>
   )
 }
