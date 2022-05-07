2c16ece0538ecf3c27c5caf373e289ded9831661
Yash Chhatrala <yashkansara0425@gmail.com>
2022-05-07 15:10:50 +0530

    part1

---
+++ react_hooks/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { useForm } from "./useForm";
+
+const App = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+
+  return (
+    <div>
+      <>
+        <input
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </>
+    </div>
+  );
+};
+
+export default App;
