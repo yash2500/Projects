41a922936e5f9094ca31ecb6e13a242ed0dd27f6
Yash Chhatrala <yashkansara0425@gmail.com>
2022-05-08 15:29:03 +0530

    Video 11

--- react_router/src/components/UserDetails.js
+++ react_router/src/components/UserDetails.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const UserDetails = () => {
+  const { userId } = useParams()
   return (
     <div>
-        <h1>User Details</h1>
+        <h1>User Details {userId}</h1>
     </div>
   )
 }
