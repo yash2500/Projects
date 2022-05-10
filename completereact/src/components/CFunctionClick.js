dedcfcddc2eac68dd74e7e0cdbb5c0562484ec59
Yash Chhatrala <yashkansara0425@gmail.com>
2022-05-10 15:04:51 +0530

    bindeventhandling

--- completereact/src/components/CFunctionClick.js
+++ completereact/src/components/CFunctionClick.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 
 export class CFunctionClick extends Component {
 
-    changeEvent(){
-        console.log('Hello Yash');
+    constructor(){
+        super();
+        this.changeEvent=this.changeEvent.bind(this);
+
+        this.state={
+            course: "I.T."
+        }
+    }
+
+    changeEvent = () =>{
+        this.setState({
+            course: "Computer"
+        })
+        console.log('Hello Yash', this.state.course);
     }
 
   render() {
     return (
       <div>
         <button onClick={this.changeEvent}>Class Click Me</button>
+        <br />
+        {this.state.course}
       </div>
     )
   }
